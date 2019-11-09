30fa6324463723ecffc363ef913ddfba120dd49a
Adson Wesley <adsonxzx@gmail.com>
2019-11-09 10:17:39 -0300

    Criando entidade registration

--- src/database/index.js
+++ src/database/index.js
@@ -4,8 +4,9 @@ import databaseConfig from '../config/database';
 import User from '../app/models/User';
 import Student from '../app/models/Student';
 import Plan from '../app/models/Plan';
+import Registration from '../app/models/Registration';
 
-const models = [User, Student, Plan];
+const models = [User, Student, Plan, Registration];
 
 /**
  * Estabelece conexao com o banco e faz Load dos models
@@ -18,6 +19,9 @@ class Database {
   init() {
     this.connection = new Sequelize(databaseConfig);
     models.map(model => model.init(this.connection));
+    models.map(
+      model => models.associate && model.associate(this.connection.models)
+    );
   }
 }
 export default new Database();
